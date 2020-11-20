8ad359a9e0d2e5e9cc924ce8396cca3597a4a63f
SteveMaverick <anway.agte@gmail.com>
2020-11-21 00:10:10 +0530

    profile page

--- app.py
+++ app.py
@@ -413,6 +413,9 @@ def convert(card):
 
     return response
 
+@app.route("/profile")
+def profile():
+    return(render_template("profile.html"))
 
 if __name__ == "__main__":
     app.run(debug=True)
